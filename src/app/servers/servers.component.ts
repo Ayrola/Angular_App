64d8c57e03e7f780dad2240687d2e3dc3c34eaf3
Ayrola97 <ayrola97@gmail.com>
2022-12-04 12:08:06 +0200

    additional practice

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowAddNewServer = false;
+  wasServerCreated = 'Server was not created';
+  serverName = '';
+  userName = '';
+  serverCreated = false;
+  canClickReset = false;
+  servers = ['testServer', 'testServer 2'];
+  details = [];
+  showDetails = false;
+constructor(){
+  setTimeout(() => {
+    this.allowAddNewServer = true;
+  }, 2000);
+}
 
+onClickAddNewServer(){
+  this.servers.push(this.serverName);
+  this.serverCreated = true;
+  this.wasServerCreated = 'New Server Was Created!' + 'Server Name: ' + this.serverName;
+}
+
+onUpdateServerName(event: Event){
+  this.serverName = (<HTMLInputElement>event.target).value;
+}
+onClickReset(){
+  this.userName = '';
+}
+onClickDetails(){
+  const dateTime = new Date();
+  this.details.push(dateTime.toUTCString());
+  this.showDetails = true;
+}
 }
